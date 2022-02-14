04d051a9cd70c887a421f8df956c0f6743003823
anil saini <aks20001005@gmail.com>
2022-02-14 19:14:50 +0530

    a

--- india_e_transport/settings.py
+++ india_e_transport/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'vedantidantwala@gmail.com'
-EMAIL_HOST_PASSWORD ='lxyqyuoktcbhpksw'
+EMAIL_HOST_USER = 'anilsainisukhpura@gmail.com'
+# EMAIL_HOST_PASSWORD ='lxyqyuoktcbhpksw'
